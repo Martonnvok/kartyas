889a0747cad61cf449edb9d788c1e5829c0dafbd
Martonnvok <martonsom@gmail.com>
2023-02-07 09:03:15 +0100

    harmadik: gép és játékos nyer-veszít

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def eredmeny(jP, gP):
-    if jP > gP and jP==21 :
-        print("Játékos nyert 21 ponttal")
+jP=10
+gP=19
+jK=[1,2,3,4,5]
+jG=[1,2,3,4,]
 
-eredmeny(jP=21,gP=10)
+def jatekos_nyer():
+    if jP>gP and jP==21:
+        print("Játékos nyert 21 ponttal")
+    elif jP>gP and len(jK)>len(jG) and jP == 19:
+        print("19 ponttal, de több lappal mint a gép")
+    elif jP > gP and len(jK) < len(jG) and jP == 19:
+        print("19 ponttal, de kevesebb lappal mint a gép")
+
+def gep_nyer():
+    if jP<gP and gP==21:
+        print("Gép nyert 21 ponttal")
+    elif jP<gP and len(jK)<len(jG) and gP == 19:
+        print("19 ponttal, de több lappal mint a Játékos")
+    elif jP < gP and len(jK) > len(jG) and gP == 19:
+        print("19 ponttal, de kevesebb lappal mint a Játékos")
+
+gep_nyer()
+jatekos_nyer()
