ddd843b81760fb5ad57c7906f337bbfe3e3a24b2
Martonnvok <martonsom@gmail.com>
2023-02-07 07:27:42 +0100

    masodik

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+def eredmeny(jP, gP):
+    if jP > gP and jP==21 :
+        print("Játékos nyert 21 ponttal")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+eredmeny(jP=21,gP=10)
